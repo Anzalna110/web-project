ed123dfab3afb24a974fa781be93b0fdbcd695d2
Anzalna110 <anzalnamasroor88@gmail.com>
2025-08-04 18:47:34 +0500

    Add files via upload

---
+++ Meet The TEam PAge/team.js
@@ -0,0 +1,11 @@
+const scrollBtn = document.getElementById("scrollToTop");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
